8133957c0b5d0b05a8444d40bd652d14f0250b08
SgmApp <shafikpin@gmail.com>
2026-08-08 00:24:48 +0530

    Update Android Project

--- app/src/main/java/com/sgmsoft/androidstudio/SplashActivity.java
+++ app/src/main/java/com/sgmsoft/androidstudio/SplashActivity.java
@@ -1,14 +1,45 @@
 package com.sgmsoft.androidstudio;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.sgmsoft.androidstudio.R;
+import android.os.Handler;
 
-public class SplashActivity extends Activity {
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        setContentView(
+            R.layout.activity_splash
+        );
+
+
+        new Handler().postDelayed(
+
+            new Runnable() {
+
+                @Override
+                public void run() {
+
+                    Intent intent =
+                        new Intent(
+						SplashActivity.this,
+						MainActivity.class
+					);
+
+                    startActivity(intent);
+
+                    finish();
+                }
+            },
+
+            SPLASH_TIME
+        );
     }
 }
